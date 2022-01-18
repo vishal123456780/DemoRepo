91db2092f431bf4e923fb6f4342db1d9ab43f361
vishal123456780 <38159338+vishal123456780@users.noreply.github.com>
2022-01-18 16:53:57 +0530

    Update SearchPage.java

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -2,7 +2,7 @@ package com.qa.pages;
 
 public class SearchPage {
 	void searchPage() {
-		System.out.println("Serach Page");
+		System.out.println("Serach Page..!!!Great coder who earns crore Rs");
 	}
 
 }
